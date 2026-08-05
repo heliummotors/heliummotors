950d3df1e7e2cb46b16db78e76d51d47e5f8cb9d
heliummotors <heliummotors08@gmail.com>
2026-08-06 01:21:04 +0400

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-window.HM_CONFIG = {
-  SUPABASE_URL: "https://fmfdemaxbxtwgpvpjbgz.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_x375Mpc8dzgz-Vx8KXtnSw_6WkfLAF5",
-  ADMIN_EMAIL: "heliummotors08@gmail.com",
-  WHATSAPP_NUMBER: "33782684607",
-  COMPANY: "Hélium Motors"
-};
